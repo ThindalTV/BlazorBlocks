e059c03f49fd4bab686cf6e935ab01ea50e5e133
ThindalTV <thindaltv@gmail.com>
2023-03-12 20:37:32 +0100

    Changing icons. Cleaning up code. Fixing multi column rendering error.

---
+++ src/BlazorBlocks/Shared/BlockEditor/Model/BlockRegistration.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorBlocks.Shared.BlockEditor.Model;
+
+public class BlockRegistration
+{
+    public string Name { get; }
+    public Type BlockModel { get; }
+    public Type EditorBlock { get; }
+
+    public BlockRegistration(string name, Type modelType, Type editorType)
+    {
+        if (!modelType.IsSubclassOf(typeof(EditorBlockBaseModel)))
+        {
+            throw new ArgumentException($"{nameof(modelType)} is not derived from {nameof(EditorBlockBaseModel)}.");
+        }
+
+        Name = name;
+        BlockModel = modelType;
+        EditorBlock = editorType;
+    }
+}
---
+++ src/BlazorBlocks/Shared/BlockEditor/Model/PolymorphicJsonConverter`1.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorBlocks.Shared.BlockEditor.Model;
+public class PolymorphicJsonConverter<T> : JsonConverter<T>
+{
+    public override bool CanConvert(Type typeToConvert)
+    {
+        return typeof(T).IsAssignableFrom(typeToConvert);
+    }
+
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+            return;
+        }
+
+        writer.WriteStartObject();
+        foreach (var property in value.GetType().GetProperties())
+        {
+            if (!property.CanRead)
+                continue;
+            var propertyValue = property.GetValue(value);
+            writer.WritePropertyName(property.Name);
+            JsonSerializer.Serialize(writer, propertyValue, options);
+        }
+        writer.WriteEndObject();
+    }
+}
