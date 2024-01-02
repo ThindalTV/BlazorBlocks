7ff73684f6c6e826020b1ef233875427f3cfea88
ThindalTV <thindaltv@gmail.com>
2024-01-02 21:53:15 +0100

    Adding todo for next time.

--- src/BlazorBlocks/Model/RowRegistration.cs
+++ src/BlazorBlocks/Model/RowRegistration.cs
@@ -3,6 +3,8 @@ public record RowRegistration
 {
     public string Name { get; }
 
+    // TODO: Add row cssclass to remove depenency on bootstrap. Currently hardcoded to "row" in EditorRowModel.Render()
+
     public IReadOnlyList<ColumnRegistration> Columns { get; }
 
     public RowRegistration(string name, IReadOnlyList<ColumnRegistration> columns)
