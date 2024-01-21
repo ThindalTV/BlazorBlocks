dd207ee7a74c353bac0c48061cdb79330ad99161
ThindalTV <thindaltv@gmail.com>
2024-01-21 19:27:54 +0100

    Fixing serialization errors.

--- src/BlazorBlocks/Model/JsonSerializerTypeResolver.cs
+++ src/BlazorBlocks/Model/JsonSerializerTypeResolver.cs
@@ -8,8 +8,12 @@ internal sealed class JsonSerializerTypeResolver : DefaultJsonTypeInfoResolver
 {
     private readonly IReadOnlyList<BlazorBlocksBlockRegistration> _blockRegistrations;
     private readonly JsonPolymorphismOptions _polymorphismOptions;
+
+    private readonly Dictionary<Type, JsonTypeInfo> _types;
     public JsonSerializerTypeResolver(IReadOnlyList<BlazorBlocksBlockRegistration> blockRegistrations)
     {
+        _types = new Dictionary<Type, JsonTypeInfo>();
+
         _blockRegistrations = blockRegistrations;
 
         var jsonTypes = _blockRegistrations.Select(reg => new JsonDerivedType(reg.BlockModel, reg.BlockModel.Name)).ToList() ?? new List<JsonDerivedType>();
@@ -29,6 +33,11 @@ internal sealed class JsonSerializerTypeResolver : DefaultJsonTypeInfoResolver
 
     public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
     {
+        if(_types.ContainsKey(type))
+        {
+            return _types[type];
+        }
+
         JsonTypeInfo jsonTypeInfo = base.GetTypeInfo(type, options);
 
         if (!_blockRegistrations.Any()) return jsonTypeInfo;
@@ -38,6 +47,8 @@ internal sealed class JsonSerializerTypeResolver : DefaultJsonTypeInfoResolver
         {
             jsonTypeInfo.PolymorphismOptions = _polymorphismOptions;
         }
+
+        _types.Add(type, jsonTypeInfo);
 
         return jsonTypeInfo;
     }
