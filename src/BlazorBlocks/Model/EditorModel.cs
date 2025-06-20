5caa97cc5981a52457b6d5b618527e3b339c0383
ThindalTV <thindaltv@gmail.com>
2025-06-20 23:15:26 +0200

    Collapse of droppablecontainer works.

--- src/BlazorBlocks/Model/EditorModel.cs
+++ src/BlazorBlocks/Model/EditorModel.cs
@@ -1,8 +1,15 @@
-﻿namespace BlazorBlocks.Model;
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorBlocks.Model;
 
 public class EditorModel
 {
+    [JsonIgnore]
     public Guid Id { get; } = Guid.NewGuid();
 
+    [JsonIgnore]
     public EditorModel? ParentModel {get; set; }
+
+    [JsonIgnore]
+    public bool Collapsed { get; set; }
 }
